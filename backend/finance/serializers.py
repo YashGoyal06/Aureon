bfd11f359c6b0ed4b4c306d4c5fc40484be169f2
YashGoyal06 <yash.goyal06@proton.me>
2026-05-18 18:39:19 +0530

    feat: add finance serializers

---
+++ backend/finance/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Transaction, Budget, Goal, Subscription
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        read_only_fields = ('user',)
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = '__all__'
+        read_only_fields = ('user', 'remaining_amount', 'status')
+
+class GoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goal
+        fields = '__all__'
+        read_only_fields = ('user',)
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
+        read_only_fields = ('user',)
